cc3f4ed0645cb4892aefca23b8bf8225959fccc7
Tibor Bodecs <mail.tib@gmail.com>
2022-01-12 22:51:22 +0100

    naming fixes

--- Sources/AggregatorModule/Controllers/Admin/AggregatorFeedAdminController.swift
+++ Sources/AggregatorModule/Controllers/Admin/AggregatorFeedAdminController.swift
@@ -64,24 +64,24 @@ struct AggregatorFeedAdminController: AdminController {
     }
     
     
-//    func detailLinks(_ req: Request, _ model: DatabaseModel) -> [LinkContext] {
+//    func detailNavigation(_ req: Request, _ model: DatabaseModel) -> [LinkContext] {
 //        [
 //            LinkContext(label: "Update",
 //                        path: "update",
 //                        permission: Blog.Author.permission(for: .update).key),
-//            LinkContext(label: BlogAuthorLinkAdminController.modelName.plural.uppercasedFirst,
+//            LinkContext(label: BlogAuthorLinkAdminController.modelName.plural,
 //                        path: Blog.Author.pathKey,
 //                        permission: Blog.Author.permission(for: .list).key),
 //        ]
 //    }
 //
-//    func updateLinks(_ req: Request, _ model: DatabaseModel) -> [LinkContext] {
+//    func updateNavigation(_ req: Request, _ model: DatabaseModel) -> [LinkContext] {
 //        [
 //            LinkContext(label: "Details",
 //                        dropLast: 1,
 //                        permission: Blog.Author.permission(for: .detail).key),
 //
-//            LinkContext(label: BlogAuthorLinkAdminController.modelName.plural.uppercasedFirst,
+//            LinkContext(label: BlogAuthorLinkAdminController.modelName.plural,
 //                        path: Blog.AuthorLink.pathKey,
 //                        dropLast: 1,
 //                        permission: Blog.AuthorLink.permission(for: .list).key),
