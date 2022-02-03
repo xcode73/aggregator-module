c12690e69f19734be7cab05c7e7e984ac7f5f7f1
Tibor Bödecs <mail.tib@gmail.com>
2022-02-03 13:53:48 +0100

    design fixes

--- Sources/AggregatorModule/Templates/Html/AggregatorFeedPageTemplate.swift
+++ Sources/AggregatorModule/Templates/Html/AggregatorFeedPageTemplate.swift
@@ -12,7 +12,7 @@ struct AggregatorFeedPageTemplate: TemplateRepresentable {
     
     var context: AggregatorFeedPageContext
     
-    init(_ context: AggregatorFeedPageContext) {    
+    init(_ context: AggregatorFeedPageContext) {
         self.context = context
     }
     
@@ -20,41 +20,44 @@ struct AggregatorFeedPageTemplate: TemplateRepresentable {
     func render(_ req: Request) -> Tag {
         WebIndexTemplate(.init(title: "Feed items")) {
             Div {
-                LeadTemplate(.init(title: "Feed items", excerpt: "Latest news from all around the world.")).render(req)
-
-                Section {
-                    for group in context.groups {
-                        Div {
-                            P {
-                                Span("\(group.items.count) item\(group.items.count > 1 ? "s" : "")")
-                                Text(" &middot; ")
-                                Text(group.day)
-                            }
-                            
-                            for item in group.items {
-                                A {
-                                    Div {
-                                        if let key = item.feed.imageKey {
-                                            Img(src: req.fs.resolve(key: key), alt: item.feed.title)
-                                                .class("profile")
-                                        }
-                                        H3(item.feed.title)
-                                        H2(item.title)
-                                    }
-                                    .class("content")
+                Div {
+                    LeadTemplate(.init(title: "Feed items", excerpt: "Latest news from all around the world.")).render(req)
+                    
+                    Section {
+                        for group in context.groups {
+                            Div {
+                                P {
+                                    Span("\(group.items.count) item\(group.items.count > 1 ? "s" : "")")
+                                    Text(" &middot; ")
+                                    Text(group.day)
                                 }
-                                .class("card")
-                                .target(.blank)
-                                .href(item.url)
+                                
+                                for item in group.items {
+                                    A {
+                                        Div {
+                                            if let key = item.feed.imageKey {
+                                                Img(src: req.fs.resolve(key: key), alt: item.feed.title)
+                                                    .class("profile")
+                                            }
+                                            H3(item.feed.title)
+                                            H2(item.title)
+                                        }
+                                        .class("content")
+                                    }
+                                    .class("card")
+                                    .target(.blank)
+                                    .href(item.url)
+                                }
+                                
                             }
-                            
+                            .class("group")
                         }
-                        .class("group")
                     }
                 }
+                .class("container")
             }
             .id("feed-items")
-            .class("container")
+            .class("wrapper")
         }
         .render(req)
     }
