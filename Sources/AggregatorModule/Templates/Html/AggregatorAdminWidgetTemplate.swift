8f63f5b8dc833cf697a92b81f65fa26c1b006610
Tibor Bodecs <mail.tib@gmail.com>
2022-02-05 11:01:12 +0100

    icon

--- Sources/AggregatorModule/Templates/Html/AggregatorAdminWidgetTemplate.swift
+++ Sources/AggregatorModule/Templates/Html/AggregatorAdminWidgetTemplate.swift
@@ -6,11 +6,13 @@
 //
 
 import SwiftHtml
+import FeatherIcons
 
 struct AggregatorAdminWidgetTemplate: TemplateRepresentable {
     
     @TagBuilder
     func render(_ req: Request) -> Tag {
+        Svg.icon(.rss)
         H2("Aggregator")
         Ul {
             if req.checkPermission(Aggregator.Feed.permission(for: .list)) {
