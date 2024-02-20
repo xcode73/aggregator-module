b2a03bcbd85d28dccb37484e65d2724d4cb10a74
xcode73 <xcode.uln@gmail.com>
2024-02-20 15:50:17 +0400

    update dependencies

--- Package.swift
+++ Package.swift
@@ -8,21 +8,21 @@ let package = Package(
     ],
     products: [
         .library(name: "AggregatorModule", targets: ["AggregatorModule"]),
-        .library(name: "AggregatorApi", targets: ["AggregatorApi"]),
+//        .library(name: "AggregatorApi", targets: ["AggregatorApi"]),
     ],
     dependencies: [
         .package(url: "https://github.com/xcode73/feather-core", .branch("test-dev")),
-        .package(url: "https://github.com/tid-kijyun/Kanna.git", from: "5.2.3"),
+        .package(url: "https://github.com/tid-kijyun/Kanna.git", from: "5.2.7"),
     ],
     targets: [
-        .target(name: "AggregatorApi", dependencies: [
-            .product(name: "FeatherCoreApi", package: "feather-core"),
-        ]),
+//        .target(name: "AggregatorApi", dependencies: [
+//            .product(name: "FeatherCoreApi", package: "feather-core"),
+//        ]),
         .target(name: "AggregatorModule", dependencies: [
             .product(name: "Feather", package: "feather-core"),
             .product(name: "Kanna", package: "Kanna"),
 
-            .target(name: "AggregatorApi"),
+//            .target(name: "AggregatorApi"),
         ],
         resources: [
 //            .copy("Bundle"),
